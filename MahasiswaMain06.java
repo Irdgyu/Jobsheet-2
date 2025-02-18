9d21edfe51ea57c87cebf40e0d34be8022967c3f
Irdgyu <gilangbayuirwana@gmail.com>
2025-02-18 13:20:40 +0700

    2

--- MahasiswaMain06.java
+++ MahasiswaMain06.java
@@ -5,10 +5,14 @@ public class MahasiswaMain06 {
         mhs1.nim = "244107020194";
         mhs1.kelas = "TI 1F SEM1";
         mhs1.ipk = 3.50;
-
+        
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("TI 1F SEM2");
         mhs1.updateIpk(3.56);
         mhs1.tampilkanInformasi();
-    }
+
+        Mahasiswa06 mhs2 = new Mahasiswa06("Annisa Nabila","2141720160",3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();;
+    }  
 }
