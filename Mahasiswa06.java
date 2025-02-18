254ca9174125f2d5ac4ab84f45cd0842078ef87d
Irdgyu <gilangbayuirwana@gmail.com>
2025-02-18 11:51:54 +0700

    1

--- Mahasiswa06.java
+++ Mahasiswa06.java
@@ -14,7 +14,11 @@ public class Mahasiswa06 {
         kelas = kelasBaru;
     }
     void updateIpk(double ipkBaru) {
-        ipk = ipkBaru;
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
     String nilaiKinerja() {
         if (ipk >= 3.5) {
---
+++ MahasiswaMain06.java
@@ -0,0 +1,14 @@
+public class MahasiswaMain06 {
+    public static void main(String[] args) {
+        Mahasiswa06 mhs1 = new Mahasiswa06();
+        mhs1.nama = "Gilang Bayu Irwana";
+        mhs1.nim = "244107020194";
+        mhs1.kelas = "TI 1F SEM1";
+        mhs1.ipk = 3.50;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("TI 1F SEM2");
+        mhs1.updateIpk(3.56);
+        mhs1.tampilkanInformasi();
+    }
+}
